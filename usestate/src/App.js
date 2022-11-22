28fcf935266a1aafe113f46f946bd12af50948f1
HAREL LEVI <harel23297@gmail.com>
2022-11-22 17:21:27 +0200

    add delta to app

--- usestate/src/App.js
+++ usestate/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import Counter from './Counter';
+import { useState } from 'react';
+
 function App() {
+  const [delta, setDelta] = useState(1)
+
+function handleDelta(e){
+  setDelta(Number(e.target.value))
+   }
   return (
     <div className="App">
       useState
-      <Counter />
+      <input type = "number" value={delta} onChange={handleDelta}/>
+      <Counter delta = {delta} />
+      <Counter delta={delta}/>
     </div>
   );
 }
--- usestate/src/Counter.js
+++ usestate/src/Counter.js
@@ -1,28 +1,27 @@
 import { useState } from "react";
-function Counter() {
-    const [count1, setCount] =useState(1) 
 
+function Counter(props) {
+    const [count1, setCount] =useState(1) 
+    const {delta} = props
+    
     function incr(){
         setCount(
             function(oldCount){
-                return oldCount+1
+                return oldCount+delta
             }
-            ) 
-        }
-
+            )  }
     function reset(){
       setCount(
         function(oldCount){
           return oldCount=1
         }
-        )   
-      }    
-
+        )    } 
+   
     return (
       <div className="App">
         <h1> Counter</h1>
          <p> Counter is at {count1}</p>
-         <button onClick={incr}> Click to add 1 to counter</button>
+         <button onClick={incr}> Click to add {delta} to counter</button>
          <button onClick={reset}> Click to reset counter</button>
         
       </div>

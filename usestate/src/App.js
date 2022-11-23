6721cd46970dfe14d6493005e450b5ee91294946
HAREL LEVI <harel23297@gmail.com>
2022-11-23 15:28:30 +0200

    adding max condition -ex3

--- usestate/src/App.js
+++ usestate/src/App.js
@@ -2,18 +2,25 @@ import './App.css';
 import Counter from './Counter';
 import { useState } from 'react';
 
+
 function App() {
   const [delta, setDelta] = useState(1)
+  const [max,setMax]=useState(1)
 
+function handelMax(e){
+  setMax(Number(e.target.value))
+}
 function handleDelta(e){
   setDelta(Number(e.target.value))
    }
   return (
     <div className="App">
-      useState
-      <input type = "number" value={delta} onChange={handleDelta}/>
-      <Counter delta = {delta} />
-      <Counter delta={delta}/>
+      useState <br/>
+      enter max value<input type = "number" value={max} onChange={handelMax}/>
+      <br/>
+      enter delta<input type = "number" value={delta} onChange={handleDelta}/>
+      <Counter delta = {delta} max={max}/>
+      <Counter delta={delta} max={max}/>
     </div>
   );
 }
--- usestate/src/Counter.js
+++ usestate/src/Counter.js
@@ -3,12 +3,17 @@ import { useState } from "react";
 function Counter(props) {
     const [count1, setCount] =useState(1) 
     const {delta} = props
+    const {max} =props
+    console.log(max)
     
     function incr(){
         setCount(
             function(oldCount){
-                return oldCount+delta
-            }
+              if (oldCount+delta >= max){
+                return oldCount=1}
+              else{
+                return oldCount+delta}
+                          }
             )  }
     function reset(){
       setCount(
